b0816f4e21fa71b212f9d21eb034bfc1705ffc42
Diogo Yaguas <diogo.yaguas@hotmail.com>
2018-12-04 12:30:48 +0000

    wip: database

--- project/mana'o/database/data.sql
+++ project/mana'o/database/data.sql
@@ -1,7 +1,11 @@
+PRAGMA foreign_keys = on;
+
 INSERT INTO User (Username, Password, Name, Email) VALUES ('diogoyaguas', 'yaguas98', 'Diogo Yaguas', 'diogo.yaguas@hotmail.com');
 INSERT INTO User (Username, Password, Name, Email) VALUES ('joanasmramos', 'joaninha1998', 'Joana Ramos', 'joanasmramos98@gmail.com');
-INSERT INTO User (Username, Password, Name, Email) VALUES ('mrpinemyapple', 'ananasevida', 'Gonçalo Nuno Bernardo', 'mrpinemyapple@gmail.com');
+INSERT INTO User (Username, Password, Name, Email) VALUES ('mrpinemyapple', 'ananasevida1998', 'Gonçalo Nuno Bernardo', 'mrpinemyapple@gmail.com');
 INSERT INTO User (Username, Password, Name, Email) VALUES ('mafaldafalcaotvf', 'professora2012', 'Mafalda Falcão', 'mafaldafferreira@fe.up.pt');
 INSERT INTO User (Username, Password, Name, Email) VALUES ('joao.malheiro', 'whiskey1998', 'João Malheiro', 'joaomalheiro@gmail.com');
-INSERT INTO User (Username, Password, Name, Email) VALUES ('smbea', 'foodie', 'Beatriz Mendes', 'smbeatriz@gmail.com');
-INSERT INTO User (Username, Password, Name, Email) VALUES ('m._.duarte', 'presinerd', 'Miguel Duarte', 'miguelduarte@gmail.com');
+INSERT INTO User (Username, Password, Name, Email) VALUES ('smbea', 'foodie98', 'Beatriz Mendes', 'smbeatriz@gmail.com');
+INSERT INTO User (Username, Password, Name, Email) VALUES ('m._.duarte', 'presinerd98', 'Miguel Duarte', 'miguelduarte@gmail.com');
+
+PRAGMA foreign_keys = on;
