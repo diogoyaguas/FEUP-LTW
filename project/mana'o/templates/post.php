382cf276674b14e5b97956d574506e2b9ee4ff87
Diogo Yaguas <diogo.yaguas@hotmail.com>
2018-12-10 23:39:11 +0000

    added posts to home

---
+++ project/mana'o/templates/post.php
@@ -0,0 +1,23 @@
+<?php
+    include_once('../includes/init.php');
+    include_once("../database/post.php");
+            function makePosts() {
+                $posts = getFiveMostRecentPosts();
+            foreach($posts as $post)
+                makePost($post);
+                unset($post);
+            }
+
+            function makePost($post) {
+                ?>
+    <div id="posts">
+        <article>
+            <h3 id="title"><?= $post['Title'] ?></h3>
+            <p id="text">
+                <?= $post['Text'] ?>
+            </p>
+            <footer><span class="author"><?php getUsername($post['User_ID']) ?></span> <span class="date"><?= $post['Date'] ?></span></footer>
+        </article>
+    </div>
+
+    <?php } ?>
